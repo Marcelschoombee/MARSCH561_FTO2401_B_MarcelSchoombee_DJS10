f0d84481e02378ed9ec839ceb9c82af324ab5c60
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-12 13:24:15 +0200

    Setting the useState

--- src/Blog.js
+++ src/Blog.js
@@ -3,4 +3,7 @@ import React, { useState, useEffect } from 'react'; // import useState and useEf
 // export Blog function
 export function Blog() {
 
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(null);
+
 }
