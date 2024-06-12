2d49b7db41d24e79a701f51dadd3b0c646b89ee1
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-12 13:29:00 +0200

    useEffect

--- src/Blog.js
+++ src/Blog.js
@@ -6,4 +6,21 @@ export function Blog() {
     const [posts, setPosts] = useState([]);
     const [error, setError] = useState(null);
 
-}
+    // useEffect hook  to pull in the data for the post
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                // Await fetch to get the API
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch posts'); // Error if post can not be fetched
+                }
+                const data = await response.json();
+                setPosts(data);
+            } catch (error) { // Catch : Shows the error message
+                setError(error.message);
+            }
+        }
+
+
+    }
